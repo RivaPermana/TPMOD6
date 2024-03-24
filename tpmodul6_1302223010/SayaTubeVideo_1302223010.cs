6dfd17700c7b907d5f065d5df8212611a4124083
shad kinx <rivapermana763@gmail,com>
2024-03-24 23:39:59 +0700

    first

---
+++ tpmodul6_1302223010/SayaTubeVideo_1302223010.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302223010
+{
+    internal class SayaTubeVideo_1302223010
+    {
+
+    }
+}
