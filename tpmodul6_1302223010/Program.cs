e38d26e2411a09b02702eb30e91d41cd20c3f722
shad kinx <rivapermana763@gmail,com>
2024-03-25 00:01:04 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302223010/Program.cs
+++ tpmodul6_1302223010/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul6_1302223010;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo_1302223010 video = null;
+
+        try
+        {
+            video = new SayaTubeVideo_1302223010("Tutorial Design By Contract - Riva Dwi Fadhillah Fajri Permana");
+            for (int i = 0; i < 10000000; i++)
+            {
+                video.IncreasePlayCount(1);
+            }
+        }
+    }
+}
--- tpmodul6_1302223010/SayaTubeVideo_1302223010.cs
+++ tpmodul6_1302223010/SayaTubeVideo_1302223010.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,24 @@ namespace tpmodul6_1302223010
 {
     internal class SayaTubeVideo_1302223010
     {
+        private int id;
+        private string title;
+        private int playCount;
 
+        public SayaTubeVideo_1302223010(string title)
+        {
+            Random ngacak = new Random();
+            id = ngacak.Next(9999, 100001);
+
+            Debug.Assert(title.Length <= 100 && title != null);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            Contract.Requires(count > 0 && count <= 10000000, "Input penambahan play count max 10.000.000 play count");
+            Contract.Ensures(playCount <= int.MaxValue - count, "Penambahan play count melebihi max");
+        }
     }
 }
